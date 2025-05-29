756e77100e900a80e98fdc186376e3a1e3bd7374
SUPRIYA MISHRA <140587863+supiimishra01@users.noreply.github.com>
2025-05-29 14:45:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function createTable() {
     //Write your code here
-	const rows = prompt("Input number of rows");
+	        const rows = prompt("Input number of rows");
             const cols = prompt("Input number of columns");
 
             const table = document.getElementById("myTable");
 
-            for (let i = 1; i <= rows; i++) {
+            for (let i = 0; i <= rows; i++) {
                 const tr = document.createElement("tr");
-                for (let j = 1; j <= cols; j++) {
+                for (let j = 0; j <= cols; j++) {
                     const td = document.createElement("td");
                     td.innerText = `Row-${i} Column-${j}`;
                     tr.appendChild(td);
